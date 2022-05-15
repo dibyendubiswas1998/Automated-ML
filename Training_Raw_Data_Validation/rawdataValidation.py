6e9b29355c00995732ad003fb47ac2a5d4524ef6
dibyendubiswas1998 <dibyendubiswas1998@gmail.com>
2022-05-15 20:12:19 +0530

    Training_Raw_Data_Validation package is created

---
+++ Training_Raw_Data_Validation/rawdataValidation.py
@@ -0,0 +1,10 @@
+
+class Raw_Data_Validation:
+    """
+        This class shall be used for validate the raw data.
+
+        Written By: Dibyendu Biswas.
+        Version: 1.0
+        Revisions: None
+    """
+    
