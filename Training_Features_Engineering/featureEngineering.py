769c1266d55f9dc2b8f9fc3ac0a3f142bd99c7c8
dibyendubiswas1998 <dibyendubiswas1998@gmail.com>
2022-05-17 09:15:15 +0530

    Removing the duplicate values from given dataset

--- Training_Features_Engineering/featureEngineering.py
+++ Training_Features_Engineering/featureEngineering.py
@@ -83,18 +83,41 @@ class Feature_Engineerings:
             raise ex
 
 
+    def ToRemoveDuplicateValues(self, data):
+        """
+            Method Name: ToRemoveDuplicateValues
+            Description: This method helps to remove the duplicate values
+
+            Output: data (after removing the duplicate values)
+            On Failure: Raise Error.
+
+            Written By: Dibyendu Biswas
+            Version: 1.0
+            Revisions: None
+        """
+        try:
+            file = open(self.file_path, 'a+')
+            self.logger_object.log(file, f"Before drop the duplicates values, the shape of data is {data.shape}")
+            data = data.drop_duplicates()
+            self.logger_object.log(file, f"After drop the duplicates values, the shape of data is {data.shape}")
+            self.logger_object.log(file, "Successfully drop the duplicates values")
+            file.close()
+            return data
+
+        except Exception as ex:
+            file = open(self.file_path, 'a+')
+            self.logger_object.log(file, f"Error is: {ex}")
+            file.close()
+            raise ex
+
+    
+
 if __name__ == '__main__':
     from Data_Ingection.data_loader import Data_Collection
     data = Data_Collection().get_data("../Raw Data/boston.csv", 'csv', separator=',')
-
-    from Training_Raw_Data_Validation.rawdataValidation import Raw_Data_Validation
-    outliers_cols = Raw_Data_Validation().IsOutliersPresent(data, data.columns, 3)
-    print(outliers_cols)
+    print("Before drop the duplicate values:  ", data.shape)
 
     featureEng = Feature_Engineerings()
-    for col in outliers_cols:
-        data = featureEng.ToHandleOutliers(data, col, 3)
+    data = featureEng.ToRemoveDuplicateValues(data)
+    print("After drop the duplicate values:  ", data.shape)
 
-    print(data, '\n\n\n\n')
-    outliers_cols = Raw_Data_Validation().IsOutliersPresent(data, data.columns, 3)
-    print(outliers_cols)
