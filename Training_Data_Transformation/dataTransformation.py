487c58e729c188f01fe6b1c8c4de493d8834984f
dibyendubiswas1998 <dibyendubiswas1998@gmail.com>
2022-05-17 18:50:09 +0530

    this method help to replace missing value with NULL vale

--- Training_Data_Transformation/dataTransformation.py
+++ Training_Data_Transformation/dataTransformation.py
@@ -20,9 +20,43 @@ class Data_Transformation:
     def __init__(self):
         self.file_path = "../Executions_Logs/Training_Logs/Data_Tansformation_Logs.txt"
         self.logger_object = App_Logger()
-    
+
+    def ToReplaceMissingWithNull(self, data):
+        """
+            Method Name: replaceMissingWithNull
+            Description: This method replaces the missing values in columns with "NULL" to
+                          store in the table.
+
+            Output: data (after replace with NULL)
+            On Failure: Raise Error.
+
+            Written By: Dibyendu Biswas.
+            Version: 1.0
+            Revisions: None
+        """
+        try:
+            file = open(self.file_path, 'a+')
+            for column in data:
+                data[column].fillna('NULL', inplace=True)
+            self.logger_object.log(file, "Successfully replace with NULL value")
+            file.close()
+            return data
+
+        except Exception as ex:
+            file = open(self.file_path, 'a+')
+            self.logger_object.log(file, f"Error is: {ex}")
+            file.close()
+            raise ex
+
 
 
 if __name__ == '__main__':
-    pass
+    from Data_Ingection.data_loader import Data_Collection
+    data = Data_Collection().get_data("../Raw Data/iris1.csv", 'csv', separator=',')
+    print(data.head(22))
+
+    dataTrans = Data_Transformation()
+    data = dataTrans.ToReplaceMissingWithNull(data)
+    print("after replacing with NULL \n\n\n", data.head(30))
+    print(data.isnull().sum())
 
