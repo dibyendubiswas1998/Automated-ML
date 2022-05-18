5cbf14700b341d20ec62decc591688ab0655d647
dibyendubiswas1998 <dibyendubiswas1998@gmail.com>
2022-05-18 07:52:17 +0530

    code descriptions are added

--- Application_Log/logger.py
+++ Application_Log/logger.py
@@ -1,15 +1,29 @@
 from datetime import datetime
 
 class App_Logger:
+    """
+        This class is responsible for log all the details with particular file.
+    """
     def __init__(self):
         pass
 
     def log(self, file_object, log_message):
+        """
+            Method Name: log
+            Description: This method log the details
+
+            Output: log the details.
+            On Failure: Raise Exception
+
+            Written By: Dibyendu Biswas
+            Version: 1.0
+            Revisions: None 
+        """
         self.now = datetime.now()
         self.date = self.now.date()
         self.current_time = self.now.strftime("%H:%M:%S")
         file_object.write(
-            str(self.date) + "/" + str(self.current_time) + "\t\t" + log_message + '\n')
+            str(self.date) + "/" + str(self.current_time) + "\t\t" + log_message + '\n')  # log the details with time stamp
 
 if __name__ == '__main__':
     logger = App_Logger()
