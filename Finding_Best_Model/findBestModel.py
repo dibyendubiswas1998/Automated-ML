c74898fe099fa3a64b79df90e805d252572b2a40
dibyendubiswas1998 <dibyendubiswas1998@gmail.com>
2022-05-19 23:34:55 +0530

    file_path rectify

--- Finding_Best_Model/findBestModel.py
+++ Finding_Best_Model/findBestModel.py
@@ -14,7 +14,7 @@ class Find_Best_Model:
          Version: 1.0
          Revisions: None
     """
-    def __init__(self, file_path="Executions_Logs/Training_Logs/Find_Best_Model_Logs.txt"):
+    def __init__(self, file_path="Executions_Logs/Training_Logs/Accuracy_Matrix_Logs.txt"):
         self.file_path = file_path  # this file path help to log the details in this file Executions_Logs/Training_Logs/Find_Best_Model_Logs.txt
         self.logger_object = App_Logger()  # call the App_Logger() to log the details
 
